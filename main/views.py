55345c45b2bff2cf1ec8dfb377a3938291f92bb4
olav.dendy <olavchristian517@gmail.com>
2024-09-25 14:36:22 +0700

    push tutorial 4

--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from main.forms import MoodEntryForm
 from main.models import MoodEntry
 from django.http import HttpResponse
@@ -14,7 +14,6 @@ from django.urls import reverse
 @login_required(login_url='/login')
 def show_main(request):
     mood_entries = MoodEntry.objects.filter(user=request.user)
-
     context = {
         'name': request.user.username,
         'class': 'PBP A',
@@ -22,18 +21,15 @@ def show_main(request):
         'mood_entries': mood_entries,
         'last_login': request.COOKIES['last_login'],
     }
-
     return render(request, "main.html", context)
 
 def create_mood_entry(request):
     form = MoodEntryForm(request.POST or None)
-
     if form.is_valid() and request.method == "POST":
         mood_entry = form.save(commit=False)
         mood_entry.user = request.user
         mood_entry.save()
         return redirect('main:show_main')
-
     context = {'form': form}
     return render(request, "create_mood_entry.html", context)
 
@@ -66,21 +62,44 @@ def register(request):
     return render(request, 'register.html', context)
 
 def login_user(request):
-   if request.method == 'POST':
+    if request.method == 'POST':
       form = AuthenticationForm(data=request.POST)
       if form.is_valid():
-        user = form.get_user()
-        login(request, user)
-        response = HttpResponseRedirect(reverse("main:show_main"))
-        response.set_cookie('last_login', str(datetime.datetime.now()))
-        return response
-   else:
+            user = form.get_user()
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_main"))
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+    else:
         form = AuthenticationForm(request)
-        context = {'form': form}
-        return render(request, 'login.html', context)
+    context = {'form': form}
+    return render(request, 'login.html', context)
 
 def logout_user(request):
     logout(request)
     response = HttpResponseRedirect(reverse('main:login'))
     response.delete_cookie('last_login')
     return response
+
+def edit_mood(request, id):
+    # Get mood entry berdasarkan id
+    mood = MoodEntry.objects.get(pk = id)
+
+    # Set mood entry sebagai instance dari form
+    form = MoodEntryForm(request.POST or None, instance=mood)
+
+    if form.is_valid() and request.method == "POST":
+        # Simpan form dan kembali ke halaman awal
+        form.save()
+        return HttpResponseRedirect(reverse('main:show_main'))
+
+    context = {'form': form}
+    return render(request, "edit_mood.html", context)
+
+def delete_mood(request, id):
+    # Get mood berdasarkan id
+    mood = MoodEntry.objects.get(pk = id)
+    # Hapus mood
+    mood.delete()
+    # Kembali ke halaman awal
+    return HttpResponseRedirect(reverse('main:show_main'))
